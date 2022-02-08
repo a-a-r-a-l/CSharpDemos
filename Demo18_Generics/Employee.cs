1333cc3d6814f8502f748aa769b1c97439135558
Riddhi Sutariya <riddhisutariya2000@gmail.com>
2022-02-08 19:00:20 +0530

    Add project files.

---
+++ Demo18_Generics/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo18_Generics
+{
+    /// <summary>
+    ///     Employee Class
+    /// </summary>
+    class Employee
+    {
+        /// <summary>
+        ///     Gets or Sets the ID of the Employee
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        ///     Gets or Sets the Name of the Employee
+        /// </summary>
+        public string EmployeeName { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("ID: {0}, Name: {1}", this.EmployeeId, this.EmployeeName);
+        }
+    }
+}
